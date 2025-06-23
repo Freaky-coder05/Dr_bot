0359f09282c29538e89866ea9029a41e86f3b75c
freaky_coder <168324938+Freaky-coder05@users.noreply.github.com>
2025-06-23 19:20:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ if you want to use an account, set use_account to True and fill in the email and
 if you want to use a guest account, set use_account to False and leave email and password empty.
 '''
 use_account = True
-Email = "losxavale69420@gmail.com " #Your email here
-Password = "gafas69.420" #Your password here
+Email = "joevaas94@gmail.com " #Your email here
+Password = "joevaas123" #Your password here
 
 # --- Telegram Settings ---
 API_ID = 25797857  # Replace with your API ID
