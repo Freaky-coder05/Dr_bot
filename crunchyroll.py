8b1a3c1f0d2866ebac94828c0331afc8ddc9e5db
freaky_coder <168324938+Freaky-coder05@users.noreply.github.com>
2025-07-27 18:50:12 +0530

    Update crunchyroll.py

--- crunchyroll.py
+++ crunchyroll.py
@@ -423,7 +423,7 @@ class CrunchyrollAuth(CrunchyrollBase):
 
     def get_user_token(self, email, password):
         endpoint = "https://www.crunchyroll.com/auth/v1/token"
-        authorization_header = "Authorization: Basic bm9haWhkZXZtXzZpeWcwYThsMHE6"
+        authorization_header = "Basic bm9haWhkZXZtXzZpeWcwYThsMHE6"
         content_type_header = "application/x-www-form-urlencoded"
         etp_anonymous_id_header = str(uuid.uuid4())
 
