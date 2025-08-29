121468a5121406aa652212a75c7c5e358622a2eb
freaky_coder <168324938+Freaky-coder05@users.noreply.github.com>
2025-08-29 19:01:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ if you want to use an account, set use_account to True and fill in the email and
 if you want to use a guest account, set use_account to False and leave email and password empty.
 '''
 use_account = True
-Email = "felipedimi123@gmail.com" #Your email here
-Password = "Zvt_105090" #Your password here
+Email = "crunchyxx2@chalonxprime.com" #Your email here
+Password = "blue123" #Your password here
 
 # --- Telegram Settings ---
 API_ID = 25797857  # Replace with your API ID
@@ -264,5 +264,6 @@ LANGUAGE_NAME_TO_ISO639_2B = {
     "Yoruba": "yor",
     "Zulu": "zul"
 }
+
 
 
